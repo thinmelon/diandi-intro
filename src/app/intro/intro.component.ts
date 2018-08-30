4af90f2b3866a76c46779f8098544e6b45969217
李云鹏 <flowerinhouse@163.com>
2018-08-30 22:09:51 +0800

    公司官网

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -6,8 +6,28 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./intro.component.less']
 })
 export class IntroComponent implements OnInit {
-    // images = ['https://warehouse.pusudo.cn/introduction/background-business-clean-811101.jpg'];
-    images = ['../../assets/map_1.jpg'];
+    entry = [
+        {
+            url: 'https://warehouse.pusudo.cn/introduction/map.jpg',
+            name: '美食地图',
+            digest: '为美食自媒体提供管理历史文章的新方式。'
+        },
+        {
+            url: 'https://warehouse.pusudo.cn/introduction/chart.jpg',
+            name: '数据图表',
+            digest: '企业办公管理新工具。'
+        },
+        {
+            url: 'https://warehouse.pusudo.cn/introduction/shop.jpg',
+            name: '线上渠道',
+            digest: '微信公众号、小程序、支付宝生活号多渠道建设。'
+        },
+        {
+            url: 'https://warehouse.pusudo.cn/introduction/development.jpg',
+            name: '定制开发',
+            digest: '需求您来定，剩下就交给我们吧。'
+        }
+    ];
 
     constructor() {
     }
