5ff8ff6832969d7c696d16411ff52756324df855
thinmelon <flowerinhouse@163.com>
2018-12-27 17:18:23 +0800

    设置图片的来源以及跳转链接

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -8,22 +8,26 @@ import {Component, OnInit} from '@angular/core';
 export class IntroComponent implements OnInit {
     entry = [
         {
-            url: 'https://warehouse.pusudo.cn/introduction/map.jpg',
+            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:5,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            src: 'https://warehouse.pusudo.cn/introduction/map.jpg',
             name: '美食地图',
             digest: '为美食自媒体提供管理历史文章的新方式。'
         },
         {
-            url: 'https://warehouse.pusudo.cn/introduction/chart.jpg',
+            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:12,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            src: 'https://warehouse.pusudo.cn/introduction/chart.jpg',
             name: '数据图表',
             digest: '企业办公管理新工具。'
         },
         {
-            url: 'https://warehouse.pusudo.cn/introduction/shop.jpg',
+            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:14,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            src: 'https://warehouse.pusudo.cn/introduction/shop.jpg',
             name: '线上渠道',
             digest: '微信公众号、小程序、支付宝生活号多渠道建设。'
         },
         {
-            url: 'https://warehouse.pusudo.cn/introduction/development.jpg',
+            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:13,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            src: 'https://warehouse.pusudo.cn/introduction/development.jpg',
             name: '定制开发',
             digest: '需求您来定，剩下就交给我们吧。'
         }
@@ -36,5 +40,9 @@ export class IntroComponent implements OnInit {
         // for (let index = 0; index < 7; index++) {
         //     this.images.push(`https://warehouse.pusudo.cn/introduction/WechatIMG${618 + index}.jpeg`);
         // }
+    }
+
+    details(url) {
+        window.location.href = url;
     }
 }
