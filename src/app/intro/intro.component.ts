d5565447982074c5d0f8cf80c71cabfdb1207a61
thinmelon <flowerinhouse@163.com>
2018-12-29 17:23:43 +0800

    更换模板的跳转链接

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -8,25 +8,25 @@ import {Component, OnInit} from '@angular/core';
 export class IntroComponent implements OnInit {
     entry = [
         {
-            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:5,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            url: 'https://backbone.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B"templateId":14,"userDesc":"美食地图","userVersion":"0.0.1","price":"1.00","shouldHavaBusiness":0%7D;channel=miniprogram',
             src: 'https://warehouse.pusudo.cn/introduction/map.jpg',
             name: '美食地图',
             digest: '为美食自媒体提供管理历史文章的新方式。'
         },
         {
-            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:12,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            url: 'https://backbone.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B"templateId":5,"userDesc":"数据统计","userVersion":"0.0.1","price":"1.00","shouldHavaBusiness":0%7D;channel=miniprogram',
             src: 'https://warehouse.pusudo.cn/introduction/chart.jpg',
             name: '数据图表',
             digest: '企业办公管理新工具。'
         },
         {
-            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:14,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            url: 'https://backbone.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B"templateId":12,"userDesc":"电子商城","userVersion":"0.0.1","price":"1.00","shouldHavaBusiness":1%7D;channel=miniprogram',
             src: 'https://warehouse.pusudo.cn/introduction/shop.jpg',
             name: '线上渠道',
             digest: '微信公众号、小程序、支付宝生活号多渠道建设。'
         },
         {
-            url: 'https://official.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B%22templateId%22:13,%22userDesc%22:%22%E7%BE%8E%E9%A3%9F%E5%9C%B0%E5%9B%BE%22,%22userVersion%22:%220.0.1%22,%22price%22:%221.00%22,%22shouldHavaBusiness%22:0%7D;channel=miniprogram',
+            url: 'https://backbone.pusudo.cn/entry/wechat/miniprogram/scenario/intro;template=%7B"templateId":5,"userDesc":"定制开发","userVersion":"0.0.1","price":"1.00","shouldHavaBusiness":0%7D;channel=miniprogram',
             src: 'https://warehouse.pusudo.cn/introduction/development.jpg',
             name: '定制开发',
             digest: '需求您来定，剩下就交给我们吧。'
